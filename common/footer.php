b5e264ff569bb3a61722aebe88e69baf8cc1e2a5
Stephen Houser <stephenhouser@gmail.com>
2017-08-02 21:39:51 -0400

    footer config with copyright

--- common/footer.php
+++ common/footer.php
@@ -1,10 +1,19 @@
 		<footer role="contentinfo">
 			<div id="footer-text">
 				<?php echo get_theme_option('Footer Text'); ?>
+
+				<p class="contact">
+					Questions or problems contact: 
+					<a href="mailto:<?php echo option('administrator_email'); ?>">
+					<?php echo option('author'); ?></a>
+				</p>
+
 				<?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = option('copyright')): ?>
-					<p><?php echo $copyright; ?></p>
+					<p class="copyright"><?php echo $copyright; ?></p>
 				<?php endif; ?>
+
 				<!-- p><?php echo __('Proudly powered by <a href="http://omeka.org">Omeka</a>.'); ?></p -->
+
 			</div>
 			<?php fire_plugin_hook('public_footer', array('view' => $this)); ?>
 		</footer>
